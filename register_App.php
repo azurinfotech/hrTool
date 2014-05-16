e6b1664753ad77e08abb2927b471f7b74f45b2ea
mrameshkumar <mrameshkumar@azurinfotech.com>
2014-05-16 12:40:16 +0530

    Changes to the Register App logic

--- register_App.php
+++ register_App.php
@@ -1,6 +1,22 @@
 <?php
+$sql="SELECT * FROM user WHERE off_email='$off_email' and password='$password'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+// If result matched $username and $password, table row must be 1 row
+if ($count > 0) {
+     die("User already exist! $count");
+}
 $emp_no = $_GET['emp_no'];
 $off_email = $_GET['off_email'];
+// validation for email
+var_dump($_POST);
+    $v = "/[a-zA-Z0-9_-.+]+@[a-zA-Z0-9-]+.[a-zA-Z]+/";
+    $valid =  (bool)preg_match($v, $off_email);
+if(!$valid){
+    die("Enter a valid Email address");
+}
 $salt = hash('sha1', 'azurinfotech');
 $password = hash('sha1', $_GET['password']) . $salt;
 $fname = $_GET['fname'];
@@ -11,6 +27,6 @@ $db = new database();
 $query = "INSERT INTO user(emp_no" . $emp_no . ",off_email" . $off_email . ",password" . $password . ",fname" . $fname . ",lname" . $lname . ",status" . $status . ")";
 $db->query_execute($query);
  
- 
 
-         
+
+        
