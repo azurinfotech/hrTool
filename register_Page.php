92f9211ab330f0d8f4acd224419a0f22ad6e9a66
ravitejam <rteja@azurinfotech.com>
2014-05-17 13:49:54 +0530

    Register form.

--- register_Page.php
+++ register_Page.php
@@ -1,60 +1,66 @@
-<html xmlns="http://www.w3.org/1999/html">
+<html>
     <head>
       <link rel="stylesheet" href="tab.css">
       <link href="foundation/css/foundation.min.css" rel="stylesheet" type="text/css"/>
       <script src="foundation/js/foundation/foundation.js" type="text/javascript"></script>
     </head>
       
+    <header></header>
+   
 <body>
-<div class="row">
-    <div class="large-9 columns" role="content">
 
-        <div id="z" class="row">
-            <div class="panel large-12 medium-6 columns" id="k">
-                <form method="POST" action="register_App.php">
-
-                    <p>Email:</p> <input type="text" style="width:200px" name="email">
-                        <p>Password:</p> <input type="text" style="width:200px" name="pass">
-                            <p>First Name:</p> <input type="text"  style="width:200px" name="fname">
-                                <p>Last Name:</p> <input type="text" style="width:200px" name="lname">
-                    <p>Status:</p>
-                    <select id="a" style="width:200px" name="status"> <option value="Active" >Active</option>
-                        <option value="Inactive" >Inactive</option>
-                    </select><br>
-                    <input type="submit" class="small radius button" value="submit">
-                </form>
+    <h3>Employee register form</h3>
+    <div id="azurinfo" class="row" align="center">
+  <div class=" large-12 medium-12 columns">
+    <form class="container1" action="loginApp.php">
+        <table style="width:500px; padding:10px;" >
+        <tr>
+                
+            <td> <input type="text"  placeholder="Email"></td>
+            <td><input type="text"  placeholder="Employee Number"></td>
+        </tr>
+        <tr>
+            <td><input type="password" placeholder="password"</td>
+            <td><a class="inline"<label>Status</label></a>
+    <select class="container"> <option value="Active" >Active</option>
+    <option value="Inactive" >Inactive</option>
+    </select></td>
+        </tr>
+        <tr>
+            <td> <input type="password" placeholder="confirm password"></td>
+            <td><input type="file"> </td>
+        </tr>
+        <tr>
+            <td><input type="text" placeholder="First Name"></td>
+            <td><input type="file" value="Upload Photo"></td>
+        </tr>
+        <tr>
+            <td> <input type="text" placeholder="Last Name"></td>
+        </tr>
+        
+        <tr>
+            <td></td>
+            <td> <a href="#" class="small radius button">Save</a></td>
+        </tr>
+               </table>
+            
+             
+            
+           
+            
+           
+   
+   
+     </form>
+>>>>>>> Stashed changes
             </div>
-
-
-    </div>
-
-    <!-- End Main Content -->
-
-
-    <!-- Sidebar -->
-
-    <aside class="large-3 columns">
-
-    </aside>
-
-    <!-- End Sidebar -->
+   
+       
+  
+    
 </div>
 
-<!-- End Main Content and Sidebar -->
 
-
-<!-- Footer -->
-
-<footer class="row">
-    <div class="large-12 columns">
-        <hr />
-        <div class="row">
-            <div class="large-6 columns">
-                <p>© Azur Infotech</p>
-            </div>
-         </div>
-    </div>
-</footer>
 
 
 </body>
